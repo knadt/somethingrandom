5f720bdc2383a21886a98541a53ffd18113bf558
Patrick Sukop <patrick.sukop@olx.com>
2022-05-12 22:06:46 +0100

    added pipeline

--- pipeline.sh
+++ pipeline.sh
@@ -1,17 +1,19 @@
 #! /bin/bash
 
+cwd=$(pwd)
+
 ###STAGE 1 - GIT CLONE###
-echo "STAGE 1: GIT CLONE"
-git clone https://github.com/knadt/somethingrandom.git /tmp/buzz
+echo "STAGE 1: GIT CLONE or COPY"
+#git clone https://github.com/knadt/somethingrandom.git /tmp/buzz
 
 ###STAGE 2 - BUILD###
 echo "STAGE 2: BUILD"
-pip3 install -r /tmp/buzz/requirements.txt
+pip3 install -r $(pwd)/requirements.txt
 
 ###STAGE 3 - TESTS###
 echo "STAGE 3: TESTS"
-python3 -m pytest -v /tmp/buzz/tests/test_generator.py > /tmp/buzz/test-artifact.txt 
-cat -n /tmp/buzz/test-artifact.txt
+python3 -m pytest -v $(pwd)/tests/test_generator.py > $(pwd)/test-artifact.txt 
+cat -n $(pwd)/test-artifact.txt
 
 ###STAGE 4 - SECURITY###
 echo "STAGE 4: Security"
@@ -19,7 +21,7 @@ if docker run --network=host \
     --rm \
     -e SONAR_HOST_URL="http://0.0.0.0:9000" \
     -e SONAR_LOGIN=${SONAR_TOKEN} \
-    -v "/tmp/buzz/:/usr/src" \
+    -v "$(pwd)/:/usr/src" \
     sonarsource/sonar-scanner-cli \
     -Dsonar.qualitygate.wait=true \
     -Dsonar.projectKey=buzz-project -Dsonar.sources=/usr/src -X ; then
@@ -30,6 +32,6 @@ else
 fi
 ###STAGE 5 - DEPLOY###
 echo "STAGE 5: Deploy"
-docker build -t buzz .         
+docker build -t buzz $(pwd)/.         
 docker run -p 5000:5000 --rm -it buzz
 
